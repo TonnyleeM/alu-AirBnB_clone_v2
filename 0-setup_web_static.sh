65d4a8bbc108a637cc7db790247af57a42e856c1
TonnyleeM <muvunyilee@gmail.com>
2025-02-08 17:40:24 +0000

    complete2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,42 +1,36 @@
 #!/usr/bin/env bash
-# sets up servers for deplyoment
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+# Sets up a web server for deployment of web_static.
+apt-get update
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
   <head>
   </head>
   <body>
-    Muvunyi TonnyLee
+    Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data/
-
-sudo echo '
-server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/Forecast;
-	location /hbnb_static {
-		alias /data/web_static/current/;
-
-}
-        index index.html index.htm 404.html index.nginx-debian.html;
-        server_name _;
-        add_header X-Served-By $HOSTNAME;
-        error_page 404 /5-design_a_beautiful_404_page.html;
-}
-
-' > /etc/nginx/sites-enabled/default
-
-sudo service nginx restart
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+service nginx restart
